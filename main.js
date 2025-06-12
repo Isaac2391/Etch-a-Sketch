90aac77d6e3ec82beae20fa08e22a769d3040fb4
Isaac Wilson <isaacrw248@gmail.com>
2025-06-12 12:45:14 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+for ( i=0; i < 16; i++) { 
+
+let div_child = document.createElement("div"); 
+let container = document.querySelector(".container-div")
+container.appendChild(div_child);
+document.getElementById(div_child).style.border = "thick solid black"; 
+
+}
+
+

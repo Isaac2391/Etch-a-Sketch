10c929794538f6da67db64aea915aaa14b8e489f
Isaac Wilson <isaacrw248@gmail.com>
2025-06-12 14:38:46 +0100

    Initial grid creation and drawing/input size

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 
-for ( i=0; i < 16; i++) { 
+const container = document.querySelector(".container-div");
 
-let div_child = document.createElement("div"); 
-let container = document.querySelector(".container-div")
+let gridSizeNum = 256
+
+function gridGenerator (amount) { 
+
+for ( let i=0; i < amount; i++) { 
+
+const div_child = document.createElement("div"); 
+div_child.textContent = " ";
 container.appendChild(div_child);
-document.getElementById(div_child).style.border = "thick solid black"; 
+div_child.style.border = "thick solid black"; 
+div_child.style.padding = "1.72%";
+div_child.style.flex = "1 1 auto";          
+div_child.style.aspectRatio = "1 / 1";      
+
+div_child.addEventListener("mouseover", () => { div_child.style.backgroundColor = "black";})
 
 }
+}
 
+gridGenerator(gridSizeNum)
+
+let input = document.querySelector("#amount_input")
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+
+    while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild); }
+
+    const value = input.value;
+    gridGenerator(value*value);
+
+  }
+});
 

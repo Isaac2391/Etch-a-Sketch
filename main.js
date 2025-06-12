fc168ca54595d95db6fb995c08747a6c8b54cec7
Isaac Wilson <isaacrw248@gmail.com>
2025-06-12 21:50:23 +0100

    Added colour and eraser options

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 
 const container = document.querySelector(".container-div");
 
+const redColour = document.querySelector(".Colour.Red");
+redColour.style.color = "red";
+const orangeColour = document.querySelector(".Colour.Orange");
+orangeColour.style.color = "orange";
+const yellowColour = document.querySelector(".Colour.Yellow");
+yellowColour.style.color = "yellow";
+const greenColour = document.querySelector(".Colour.Green");
+greenColour.style.color = "green";
+const blueColour = document.querySelector(".Colour.Blue");
+blueColour.style.color = "blue";
+const indigoColour = document.querySelector(".Colour.Indigo");
+indigoColour.style.color = "indigo";
+const eraserColour = document.querySelector(".Eraser")
+
+
+redColour.addEventListener("click", () => { mainColorVar = "red" });
+orangeColour.addEventListener("click", () => { mainColorVar = "orange" });
+yellowColour.addEventListener("click", () => { mainColorVar = "yellow" });
+greenColour.addEventListener("click", () => { mainColorVar = "green" });
+blueColour.addEventListener("click", () => { mainColorVar = "blue" });
+indigoColour.addEventListener("click", () => { mainColorVar = "indigo" });
+eraserColour.addEventListener("click", () => { mainColorVar = "white" });
+
 let gridSizeNum = 256
 
 function gridGenerator (amount) { 
@@ -10,15 +33,15 @@ for ( let i=0; i < amount; i++) {
 const div_child = document.createElement("div"); 
 div_child.textContent = " ";
 container.appendChild(div_child);
-div_child.style.border = "thick solid black"; 
-div_child.style.padding = "1.72%";
-div_child.style.flex = "1 1 auto min-width: 0;";          
-div_child.style.aspectRatio = "1 / 1";      
 
-div_child.addEventListener("mouseover", () => { div_child.style.backgroundColor = "black";})
+div_child.style.border = "thick solid black";
+div_child.style.borderWidth = "2.5px" 
+div_child.style.flex = "1 0 auto min-width: 0;";
+div_child.style.padding = "1%"
 
-}
-}
+div_child.addEventListener("mouseover", () => { div_child.style.backgroundColor = mainColorVar;})
+
+}}
 
 gridGenerator(gridSizeNum)
 
@@ -31,6 +54,7 @@ input.addEventListener("keydown", (event) => {
     container.removeChild(container.firstChild); }
 
     const value = input.value;
+
     gridGenerator(value*value);
 
   }

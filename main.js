50ed5cdec0edc30a94c1da2444854e2642b68d21
Isaac Wilson <isaacrw248@gmail.com>
2025-06-15 05:06:43 +0100

    Added paint toggle and max grid size

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 
+let ColourTrue = false;
+
 const container = document.querySelector(".container-div");
+
+document.addEventListener("keydown", () => { 
+  ColourTrue = false 
+});
+
+document.addEventListener("keyup", () => { 
+  ColourTrue = true; 
+});
+
 
 const redColour = document.querySelector(".Colour.Red");
 redColour.style.color = "red";
@@ -15,21 +26,20 @@ const indigoColour = document.querySelector(".Colour.Indigo");
 indigoColour.style.color = "indigo";
 const eraserColour = document.querySelector(".Eraser")
 
-
-redColour.addEventListener("click", () => { mainColorVar = "red" });
-orangeColour.addEventListener("click", () => { mainColorVar = "orange" });
-yellowColour.addEventListener("click", () => { mainColorVar = "yellow" });
-greenColour.addEventListener("click", () => { mainColorVar = "green" });
-blueColour.addEventListener("click", () => { mainColorVar = "blue" });
-indigoColour.addEventListener("click", () => { mainColorVar = "indigo" });
-eraserColour.addEventListener("click", () => { mainColorVar = "white" });
+redColour.addEventListener("click", () => {ColourTrue = true, mainColorVar = "red" });
+orangeColour.addEventListener("click", () => { ColourTrue = true, mainColorVar = "orange" });
+yellowColour.addEventListener("click", () => {  ColourTrue = true, mainColorVar = "yellow" });
+greenColour.addEventListener("click", () => { ColourTrue = true ,mainColorVar = "green" });
+blueColour.addEventListener("click", () => { ColourTrue = true ,mainColorVar = "blue" });
+indigoColour.addEventListener("click", () => { ColourTrue = true ,mainColorVar = "indigo" });
+eraserColour.addEventListener("click", () => { ColourTrue = true , mainColorVar = "white" });
 
 let gridSizeNum = 256
 
-function gridGenerator (amount) { 
+function gridGenerator(amount){ 
 
 for ( let i=0; i < amount; i++) { 
-
+ 
 const div_child = document.createElement("div"); 
 div_child.textContent = " ";
 container.appendChild(div_child);
@@ -39,7 +49,7 @@ div_child.style.borderWidth = "2.5px"
 div_child.style.flex = "1 0 auto min-width: 0;";
 div_child.style.padding = "1%"
 
-div_child.addEventListener("mouseover", () => { div_child.style.backgroundColor = mainColorVar;})
+div_child.addEventListener("mouseover", () => { if (ColourTrue === true) { div_child.style.backgroundColor = mainColorVar };})
 
 }}
 
@@ -48,7 +58,7 @@ gridGenerator(gridSizeNum)
 let input = document.querySelector("#amount_input")
 
 input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && input.value < 1001) {
 
     while (container.hasChildNodes()) {
     container.removeChild(container.firstChild); }
